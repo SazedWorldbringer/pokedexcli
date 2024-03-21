67ebc601b933f6d80931eb1dea2b8859e382c6cd
Atharva Pardeshi <atharva.exe@gmail.com>
2024-03-21 20:22:04 +0530

    feat: define basic commands, run repl and read for input

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+var commands = map[string]cliCommand{
+	"help": {
+		name:        "help",
+		description: "Display help message",
+		callback:    commandHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
+func commandHelp() {
+	fmt.Print(`
+Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+
+`)
+}
+
+func commandExit() {
+	// exit the repl
+	os.Exit(0)
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	for true {
+		scanner := bufio.NewScanner(os.Stdin)
+
+		// read for input
+		fmt.Print("Pokedex> ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		// execute command
+		for _, command := range commands {
+			if input == command.name {
+				command.callback()
+			}
+		}
+	}
 }
