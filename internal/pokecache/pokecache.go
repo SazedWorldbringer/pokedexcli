c80db781e05e4ef91c8adb6b4056ef2921cba37a
Atharva Pardeshi <atharva.exe@gmail.com>
2024-03-28 10:30:49 +0530

    feat: write reap and reaploop methods

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -8,27 +8,47 @@ type cacheEntry struct {
 }
 
 type Cache struct {
-	entries map[string]cacheEntry
+	cache map[string]cacheEntry
 }
 
+// Create a new cache
 func NewCache(interval time.Duration) Cache {
 	c := Cache{
-		entries: make(map[string]cacheEntry),
+		cache: make(map[string]cacheEntry),
 	}
+	go c.reapLoop(interval)
+
 	return c
 }
 
+// Add a cache entry
 func (c *Cache) Add(key string, val []byte) {
-	c.entries[key] = cacheEntry{
+	c.cache[key] = cacheEntry{
 		createdAt: time.Now(),
 		val:       val,
 	}
 }
 
+// Get a cache entry
 func (c *Cache) Get(key string) ([]byte, bool) {
-	entry, ok := c.entries[key]
+	entry, ok := c.cache[key]
 	if !ok {
 		return []byte{}, false
 	}
 	return entry.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	for key, value := range c.cache {
+		if value.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, key)
+		}
+	}
 }
