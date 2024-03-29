ed1fdf30ada1c4d2057d4245c6767f9847c28153
Atharva Pardeshi <atharva.exe@gmail.com>
2024-03-29 17:22:21 +0530

    fix: function signature feat: implement explore command

--- cmd_explore.go
+++ cmd_explore.go
@@ -1,5 +1,25 @@
 package main
 
-func commandExplore(cfg *config, arg string) error {
+import (
+	"errors"
+	"fmt"
+)
+
+func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New(`you must provide a location name
+usage: explore <location_name>
+`)
+	}
+
+	areaName := args[0]
+	locationRes, err := cfg.pokeapiClient.GetLocation(areaName)
+	if err != nil {
+		return err
+	}
+
+	for _, encounter := range locationRes.PokemonEncounters {
+		fmt.Println(encounter.Pokemon.Name)
+	}
 	return nil
 }
