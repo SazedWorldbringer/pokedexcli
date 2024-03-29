f70c62fc0b962e5c81fc9c77ffd495c48ba86723
Atharva Pardeshi <atharva.exe@gmail.com>
2024-03-29 17:21:30 +0530

    feat: api handler to get more info of a specific location

---
+++ internal/pokeapi/location_get.go
@@ -0,0 +1,48 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	// "fmt"
+	"io"
+	"net/http"
+)
+
+func (c *Client) GetLocation(areaName string) (Location, error) {
+	url := baseURL + "/location-area/" + areaName
+
+	if body, ok := c.cache.Get(url); ok {
+		locationRes := Location{}
+		err := json.Unmarshal(body, &locationRes)
+		if err != nil {
+			return Location{}, err
+		}
+		return locationRes, nil
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return Location{}, err
+	}
+
+	// make get request
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Location{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Location{}, err
+	}
+
+	// parse json data
+	locationRes := Location{}
+	err = json.Unmarshal(body, &locationRes)
+	if err != nil {
+		return Location{}, err
+	}
+
+	c.cache.Add(url, body)
+	return locationRes, nil
+}
