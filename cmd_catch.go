b4fafe94e7d2d293f4bf4424d1ae4e0c9de526f8
Atharva Pardeshi <atharva.exe@gmail.com>
2024-03-30 11:30:23 +0530

    feat: implement catch command, store caught pokemon in pokedex

---
+++ cmd_catch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New(`you must provide a Pokemon name
+usage: catch <pokemon_name>
+`)
+	}
+
+	name := args[0]
+	pokemon, err := cfg.pokeapiClient.GetPokemon(name)
+	if err != nil {
+		return err
+	}
+
+	toughness := rand.Intn(pokemon.BaseExperience)
+
+	fmt.Printf("Throwing a pokeball at %s...\n", name)
+	if toughness > 50 {
+		fmt.Printf("%s escaped!\n", pokemon.Name)
+		return nil
+	}
+	fmt.Printf("%s was caught!\n", pokemon.Name)
+
+	cfg.caughtPokemon[pokemon.Name] = pokemon
+	return nil
+}
